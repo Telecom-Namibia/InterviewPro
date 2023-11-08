22b655f4b20500e59e0ffee4350cb3f2e5f4f73d
hpmouton <hubertthedev@gmail.com>
2023-11-08 12:24:06 +0200

    Shortlisted Candidates Section added

--- App.tsx
+++ App.tsx
@@ -5,14 +5,14 @@ import TabNavigation from './app/navigations/TabNavigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import type {StatusBarStyle} from 'react-native';
+import VacancyTabNavigator from './app/navigations/CandidatesTabNavigator';
 
 
 export default function App() {
 return(
 
     <AuthProvider>
-        <Layout>
-         </Layout>
+        <Layout></Layout>
         
     </AuthProvider>
 
@@ -28,6 +28,7 @@ export const Layout = () => {
 
             {/* {authState?.authenticated ? ( */}
                 <TabNavigation/>
+
 
             {/* ) : (
                 <Login/>
