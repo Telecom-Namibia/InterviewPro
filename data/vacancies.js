f718ffe045d5fc992175987f7fed7c8a27c07a1f
hpmouton <hubertthedev@gmail.com>
2023-11-08 12:24:06 +0200

    Shortlisted Candidates Section added

---
+++ data/vacancies.js
@@ -0,0 +1,60 @@
+
+
+export const vacancies = [
+  { id: 1, title: "Software Engineer" },
+  { id: 2, title: "Product Manager" },
+  { id: 3, title: "Marketing Specialist" },
+  { id: 4, title: "Data Scientist" }, // Added
+  { id: 5, title: "UX Designer" }, // Added
+];
+
+export const candidates = {
+  1: [
+    { id: 101, name: "John Doe", grade: 90 },
+    { id: 102, name: "Jane Smith", grade: 85 },
+    { id: 103, name: "Charlie Brown", grade: 88 },
+    { id: 501, name: "Jack Brown", grade: 93 },
+    { id: 502, name: "Karen Smith", grade: 94 },
+    { id: 503, name: "Linda Johnson", grade: 88 },
+    { id: 504, name: "Michael Davis", grade: 90 },
+    { id: 505, name: "Nora Wilson", grade: 91 }, // Added
+  ],
+  2: [
+    { id: 201, name: "Alice Johnson", grade: 95 },
+    { id: 202, name: "Bob Brown", grade: 80 },
+    { id: 203, name: "David Smith", grade: 92 },
+    { id: 101, name: "John Doe", grade: 90 },
+    { id: 102, name: "Jane Smith", grade: 85 },
+    { id: 103, name: "Charlie Brown", grade: 88 },
+    { id: 104, name: "Diana Johnson", grade: 92 },
+    { id: 105, name: "Evan Davis", grade: 86 },
+    { id: 106, name: "Grace Wilson", grade: 78 },
+    { id: 107, name: "Henry White", grade: 79 },
+    { id: 108, name: "Isabella Brown", grade: 75 },
+    { id: 109, name: "Jack Smith", grade: 70 },
+    { id: 110, name: "Katherine Johnson", grade: 68 }, // Added
+  ],
+  3: [
+    { id: 301, name: "Ella Davis", grade: 90 },
+    { id: 302, name: "Frank Wilson", grade: 85 },
+    { id: 303, name: "George Johnson", grade: 89 }, // Added
+  ],
+  4: [
+    { id: 401, name: "Helen Davis", grade: 91 }, // Added
+    { id: 402, name: "Irene Wilson", grade: 87 }, // Added
+  ],
+  5: [
+    { id: 501, name: "Jack Brown", grade: 93 }, // Added
+    { id: 502, name: "Karen Smith", grade: 94 }, 
+    { id: 401, name: "Helen Davis", grade: 91 },
+    { id: 402, name: "Irene Wilson", grade: 87 },
+    { id: 403, name: "Jacob Smith", grade: 92 },
+    { id: 404, name: "Katherine Johnson", grade: 89 },
+    { id: 405, name: "Liam Brown", grade: 95 },
+    { id: 406, name: "Mia Davis", grade: 78 },
+    { id: 407, name: "Noah Smith", grade: 79 },
+    { id: 408, name: "Olivia Wilson", grade: 74 },
+    { id: 409, name: "Peter Brown", grade: 70 },
+    { id: 410, name: "Quinn Johnson", grade: 68 },// Added
+  ],
+};
