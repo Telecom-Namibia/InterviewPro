75553887ec999a855100affdeb4ac37646460cdb
aaron-muti0420 <mutiaaron2@gmail.com>
2023-11-09 11:04:26 +0200

    button

--- app/components/Vacancies/VacancyHeader.tsx
+++ app/components/Vacancies/VacancyHeader.tsx
@@ -1,24 +1,47 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
 import { Entypo } from '@expo/vector-icons';
 
 interface Props {
   onPressAddVacancy: () => void;
 }
 
-const VacancyHeader = ({ onPressAddVacancy }: Props) => {
+const VacancyHeader: React.FC<Props> = ({ onPressAddVacancy }) => {
   return (
-    <View style={{ justifyContent: 'center', paddingTop: 54, paddingBottom: 27, paddingHorizontal: 20, backgroundColor: '#1F2F47', borderBottomRightRadius: 20, borderBottomLeftRadius: 20 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Text style={{ color: '#fff', fontSize: 21, fontWeight: '600' }}>Current Vacancies</Text>
-        <TouchableOpacity onPress={onPressAddVacancy}>
+    <View style={styles.container}>
+      <View style={styles.headerContent}>
+        <Text style={styles.headerText}>Current Vacancies</Text>
+        <TouchableOpacity style={styles.button} onPress={onPressAddVacancy}>
           <Entypo name="add-to-list" size={24} color="#fff" />
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default VacancyHeader
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    paddingTop: 54,
+    paddingBottom: 27,
+    paddingHorizontal: 20,
+    backgroundColor: '#1F2F47',
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  headerContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 21,
+    fontWeight: '600',
+  },
+  button: {
+    padding: 10,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default VacancyHeader;
